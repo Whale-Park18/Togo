515243cfe193f27b8bf31134490b63f176355546
Whale-Park18 <whale.park18@gmail.com>
2022-01-27 16:00:04 +0900

    togo::cli::command.h 수정
    * AbstructCommand* CommandFilt(...) : inline 키워드 추가
        * old: AbstructCommand* CommandFilt(...)
        * new inline AbstructCommand* CommandFilt(...)

--- Togo/cli/command.h
+++ Togo/cli/command.h
@@ -11,25 +11,25 @@ namespace togo {
 namespace cli {
 namespace command {
 
-	AbstractCommand* CommandFilt(std::string command, std::map<std::string, std::string> input_options_mapping)
-	{
-		AbstractCommand* command_master;
+inline AbstractCommand* CommandFilt(std::string command, std::map<std::string, std::string> input_options_mapping)
+{
+	AbstractCommand* command_master;
 
-		if (command._Equal("add"))
-			command_master = new AddCommand(input_options_mapping);
-		else if (command._Equal("run"))
-			command_master = new BackupCommand(input_options_mapping);
-		else if (command._Equal("list"))
-			command_master = new ListCommand(input_options_mapping);
-		else if (command._Equal("rm"))
-			command_master = new RemoveCommand(input_options_mapping);
-		else if (command._Equal("set"))
-			command_master = new SettingCommand(input_options_mapping);
-		else
-			command_master = nullptr;
+	if (command._Equal("add"))
+		command_master = new AddCommand(input_options_mapping);
+	else if (command._Equal("run"))
+		command_master = new BackupCommand(input_options_mapping);
+	else if (command._Equal("list"))
+		command_master = new ListCommand(input_options_mapping);
+	else if (command._Equal("rm"))
+		command_master = new RemoveCommand(input_options_mapping);
+	else if (command._Equal("set"))
+		command_master = new SettingCommand(input_options_mapping);
+	else
+		command_master = nullptr;
 
-		return command_master;
-	}
+	return command_master;
+}
 
 } // namespace command
 } // namespace cli
